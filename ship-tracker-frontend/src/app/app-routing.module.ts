083ef545ce96c24a699ff81f58faa101ce0706bf
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-01-20 01:10:56 +0100

    feat Added component to main page and routing

--- ship-tracker-frontend/src/app/app-routing.module.ts
+++ ship-tracker-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LoggedPageComponent } from './logged-page/logged-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'logged', component: LoggedPageComponent },
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ship-tracker-frontend/src/app/app.component.ts
+++ ship-tracker-frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ship-tracker-frontend';
+export class AppComponent implements OnInit {
+
+  constructor(private _router: Router) { }
+
+  title = 'ship tracker';
+
+  ngOnInit() {
+    if(localStorage.getItem('token') === null) {
+      this._router.navigate(['/login']);
+      localStorage.setItem('token', '1234567890');
+      console.log('token set');
+    }
+    else{
+      this._router.navigate(['/logged']);
+    }
+    let token = localStorage.getItem('token');
+    console.log(token);
+  } 
 }
