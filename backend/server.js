b938021c35900ab94cbdd85f85adeb64f5e7f6d1
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-01-28 13:18:01 +0100

    Fix Added Access-Control-Allow-Methods

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,12 @@ const port = 3000;
 //second, start server on specified port
 app.listen(port, () => console.log("Express server running on port " + port))
 
-const loginRouter = require('./routes/login')
+const loginRouter = require('./routes/Login')
 const shipRouter = require('./routes/Ship_handler')
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
 app.use('/login', loginRouter)
