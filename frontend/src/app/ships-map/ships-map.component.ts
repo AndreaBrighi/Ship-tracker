579c8947ceca5ad54daa6f5e0a4359202a5b0205
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-08-16 19:24:58 +0200

    fix(frontend): update map component

--- frontend/src/app/ships-map/ships-map.component.ts
+++ frontend/src/app/ships-map/ships-map.component.ts
@@ -1,9 +1,12 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
 import { Observable, timer } from 'rxjs';
 import {ship, coordinates } from '../../data/ship';
 import query from '../../data/query';
+import { BackendService } from '../backend.service';
+import { LoggerService } from '../logger.service';
+import { Router } from '@angular/router';
 
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
@@ -27,67 +30,89 @@ L.Marker.prototype.options.icon = iconDefault;
 })
 export class ShipsMapComponent implements AfterViewInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private backend: BackendService, private login: LoggerService, private _router: Router) { }
 
   private map: L.Map | undefined;
   private markers = new Map<ship, L.Marker>();
   private tick: any;
+  private showAllarm = false;
 
   private initMap(): void {
-      const initialState = { lng: 11, lat: 49, zoom: 4 };
-  
-      const lefletMap: L.Map = L.map('map').setView(
-          [initialState.lat, initialState.lng],
-          initialState.zoom
-      );
-  
-      this.map = lefletMap;
-  
-      const isRetina = L.Browser.retina;
-      const baseUrl =
-        'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey={apiKey}';
-      const retinaUrl =
-        'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey={apiKey}';
-  
-      L.tileLayer(isRetina ? retinaUrl : baseUrl, {
-        attribution:
-          'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
-        // This API key is for use only in stackblitz.com
-        // Get your Geoapify API key on https://www.geoapify.com/get-started-with-maps-api
-        // The Geoapify service is free for small projects and the development phase.
-        apiKey: 'abe6874472c0421ea555b3af62758aba',
-        maxZoom: 20,
-        id: 'osm-bright',
-      } as any).addTo(lefletMap);
+    this._router.url === '/watcher' ? this.showAllarm = true : this.showAllarm = false;
+    console.log(this._router.url);
+    this.getPosition().then((pos) => {
+      console.log(pos);
+      return {lat: pos.latitude, lng: pos.longitude, zoom: 10};
+    }).catch((err) => {
+      console.log(err);
+      return{ lat: 51.505, lng: -0.09, zoom: 4 };
+    }).then((initialState) => {
+        const lefletMap: L.Map = L.map('map').setView(
+            [initialState.lat, initialState.lng],
+            initialState.zoom
+        );
+    
+        this.map = lefletMap;
+    
+        const isRetina = L.Browser.retina;
+        const baseUrl =
+          'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey={apiKey}';
+        const retinaUrl =
+          'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey={apiKey}';
+    
+        L.tileLayer(isRetina ? retinaUrl : baseUrl, {
+          attribution:
+            'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
+          // This API key is for use only in stackblitz.com
+          // Get your Geoapify API key on https://www.geoapify.com/get-started-with-maps-api
+          // The Geoapify service is free for small projects and the development phase.
+          apiKey: 'abe6874472c0421ea555b3af62758aba',
+          maxZoom: 20,
+          id: 'osm-bright',
+        } as any).addTo(lefletMap);
+      });
     }
   
     updatesMarkers(map: L.Map, ships: ship[]): void {
       console.log(ships);
       let moved = 
       Array.from(this.markers.keys())
-      .filter((i) => ships.find((j) => i.name === j.name && (i.actual_position.x !== j.actual_position.x || i.actual_position.y !== j.actual_position.y)) );
-      console.log(moved);
+      .filter((i) => ships.find((j) => i.name === j.name && (i.actual_position.latitude !== j.actual_position.latitude || 
+        i.actual_position.longitude !== j.actual_position.longitude)) );
       moved.forEach((i) => {
         let marker = this.markers.get(i);
         const ship = ships.find((j) => i.name === j.name);
         if (marker && ship) {
+          let newMarker = this.creteMarker(ship);
+          if(marker.isPopupOpen()) {
+            newMarker.openPopup();
+          }
           this.map!.removeLayer(marker);
           this.markers.delete(i);
-          let newMarker = L.marker([ship.actual_position.x, ship.actual_position.y]);
-          newMarker.addTo(map);
-          newMarker.bindTooltip(`<p>${i.name}</p>`);
           this.markers.set(ship, newMarker);
+
         }
       });
-      let newShips = ships.filter((i) => !this.markers.has(i));
+      let newShips = ships.filter((i) => Array.from(this.markers.keys()).find((j) => i.name === j.name) === undefined);
       newShips.forEach((i) => {
-        let marker = L.marker([i.actual_position.x, i.actual_position.y]);
-        marker.addTo(map);
-        marker.bindTooltip(`<p>${i.name}</p>`);
+        let marker = this.creteMarker(i);
         this.markers.set(i, marker);
       }
       );
       
+  }
+
+  private creteMarker(ship: ship): L.Marker {
+    let marker = L.marker([ship.actual_position.latitude, ship.actual_position.longitude]);
+    marker.addTo(this.map!);
+    marker.bindPopup(`
+    <p>${ship.name}</p>
+    <p>${ship.actual_position.latitude}</p>
+    <p>${ship.actual_position.longitude}</p>
+    <p>${ship.status}</p>
+    `);
+    marker.bindTooltip("<b>" + ship.name + "</b>");
+    return marker;
   }
 
   ngAfterViewInit(): void {
@@ -95,19 +120,28 @@ export class ShipsMapComponent implements AfterViewInit {
     const source = timer(0, 500);
     this.tick = source.subscribe(val => {
       console.log(val, '-');
-      this.http.get<query<ship>>('http://localhost:3000/shipreq/getall')
+      this.backend.getShips()
       .subscribe(
         (data) => {
           console.log(data);
-          if(data.found > 0) {
-            //this.remove();
-            this.updatesMarkers(this.map!, data.payload);
-          }
+          this.updatesMarkers(this.map!, data);
         });
     })
   }
 
   ngOnDestroy(): void {
     this.tick.unsubscribe();
+  }
+
+  private async getPosition(): Promise<coordinates>
+  {
+    return await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resp => {
+          resolve({longitude: resp.coords.longitude, latitude: resp.coords.latitude});
+        },
+        err => {
+          reject(err);
+        });
+    });
   }
 }
