da906a0fdcc4b11a6ec57cef600352ab5bbd7a15
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-09-03 03:42:44 +0200

    feat(frontend): add new-ship page

---
+++ frontend/src/app/new-ship/new-ship.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatSelectModule} from '@angular/material/select';
+import { LoggerService } from '../logger.service';
+import { BackendService } from '../backend.service';
+import { catchError } from 'rxjs';
+
+@Component({
+  selector: 'app-new-ship',
+  templateUrl: './new-ship.component.html',
+  styleUrls: ['./new-ship.component.scss']
+})
+export class NewShipComponent {
+addShip() {
+throw new Error('Method not implemented.');
+}
+newShipName: String = '';
+routes: String[] = [];
+latitude: number = 0.0;
+longitude: number = 0.0;
+
+constructor(private _router: Router, private http: HttpClient, private loggerService: LoggerService, private backendService: BackendService) {
+  this.backendService.getAllRoutes()
+  .pipe(
+    catchError((err) => {
+      console.log('error');
+      console.log(err);
+      return [];
+    }))
+    .subscribe(
+      (data) => {
+        console.log('data');
+        console.log(data);
+        this.routes = data.payload.map((route: any) => route.name as String);
+      }
+    )
+ }
+
+}
